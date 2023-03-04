661130e5cf3406e89a64536c664ddb8b3a2eae1a
Nazario A. Ayala <nazario@niztech.com>
2023-03-03 20:42:57 -0800

    Stubbed out Aircraft controller

---
+++ src/Controllers/aircraftController.ts
@@ -0,0 +1,41 @@
+import { RouterContext } from 'oak/mod.ts'
+import { doTheDatabase } from '../sqlight.ts';
+import { Aircraft } from '../models.ts'
+
+export const getAllAircraft = async (ctx: RouterContext) => {
+	ctx.response.body = await Aircraft.all()
+}
+
+export const createAircraft = async (ctx: RouterContext) => {
+	const { nNumber, make, model } = await ctx.request.body().value
+
+  const id = await aircraftsCollection.insertOne(aircraftEntry)
+  ctx.response.status = 201
+  ctx.response.body = {id}
+}
+
+export const getAircraft = async (ctx: RouterContext) => {
+	const nNumber = await ctx.request.body().value
+	const aircraft = await Aircraft.find({ nNumber })
+	if (aircraft) {
+		ctx.response.status = 200
+		ctx.response.body = { aircraft }
+	} else {
+		ctx.response.status = 404
+	}
+}
+
+export const updateAircraft = async (ctx: RouterContext) => {
+  await doTheDatabase()
+
+  const aircraftId = await ctx.request.body().value;
+  // update aircraft in DB
+  ctx.response.status = 201
+  ctx.response.body = {aircraftId}
+}
+
+export const deleteAircraft = async (ctx: RouterContext) => {
+	const nNumber = await ctx.request.body().value
+	const aircraft = await Aircraft.deleteById(nNumber)
+	ctx.response.status = 200
+}
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,13 @@
 import { Application, Router, RouterContext } from './deps.ts'
+import { createFlight, deleteFlight, getAllFlights, getFlight, updateFlight, } from './Controllers/flightController.ts'
+import { createPilot, deletePilot, getAllPilots, getPilot, updatePilot, } from './Controllers/pilotController.ts'
 import {
-	createFlight,
-	deleteFlight,
-	getAllFlights,
-	getFlight,
-	updateFlight,
-} from './Controllers/flightController.ts'
-import {
-	createPilot,
-	deletePilot,
-	getAllPilots,
-	getPilot,
-	updatePilot,
-} from './Controllers/pilotController.ts'
+  createAircraft,
+  deleteAircraft,
+  getAircraft,
+  getAllAircraft,
+  updateAircraft
+} from './Controllers/aircraftController.ts'
 
 const app = new Application()
 const router = new Router()
@@ -21,18 +16,24 @@ const port: number = 8000
 router.get('/', (ctx: RouterContext) => {
 	ctx.response.body = 'Hello from Deno'
 })
-	// flight
-	.get('/flights', getAllFlights)
-	.get('/flight/:id', getFlight)
-	.post('/flight', createFlight)
-	.put('/flight/:id', updateFlight)
-	.delete('/flight/:id', deleteFlight)
-	// pilot
-	.get('/pilots', getAllPilots)
-	.get('/pilot/:id', getPilot)
-	.post('/pilot', createPilot)
-	.put('/pilot/:id', updatePilot)
-	.delete('/pilot/:id', deletePilot)
+  // flight
+  .get('/flights', getAllFlights)
+  .get('/flight/:id', getFlight)
+  .post('/flight', createFlight)
+  .put('/flight/:id', updateFlight)
+  .delete('/flight/:id', deleteFlight)
+  // pilot
+  .get('/pilots', getAllPilots)
+  .get('/pilot/:id', getPilot)
+  .post('/pilot', createPilot)
+  .put('/pilot/:id', updatePilot)
+  .delete('/pilot/:id', deletePilot)
+  // aircraft
+  .get('/aircraft', getAllAircraft)
+  .get('/aircraft/:id', getAircraft)
+  .post('/aircraft', createAircraft)
+  .put('/aircraft/:id', updateAircraft)
+  .delete('/aircraft/:id', deleteAircraft)
 
 // Here, we are telling our application to use the router
 app.use(router.routes())
