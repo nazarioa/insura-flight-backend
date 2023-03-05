37cc6f9b68021c902e21dd62713188de4d102a56
Nazario A. Ayala <nazario@niztech.com>
2023-03-04 23:28:30 -0800

    Broke flight model out

--- src/sqlight.ts
+++ src/sqlight.ts
@@ -1,7 +1,7 @@
-import { Flight } from './models.ts'
 import { Database, SQLite3Connector } from 'denodb/mod.ts'
 import { Pilot } from './Models/Pilot.ts'
 import { Aircraft } from './Models/Aircraft.ts'
+import { Flight } from './Models/Flight.ts'
 
 const connector = new SQLite3Connector({
 	filepath: 'db/flight-database.sqlite',
