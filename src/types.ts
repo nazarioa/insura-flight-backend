b38033b9d2aa98691cf68735b335914f2f98c81c
Nazario A. Ayala <nazario@niztech.com>
2023-03-04 23:34:36 -0800

    fixup! First pass at types

--- src/types.ts
+++ src/types.ts
@@ -1,30 +1,12 @@
 export interface FlightEntry {
-	/*
-  pilotDetails: Pilot
-  flightStart: FlightNode
-  flightEnd: FlightNode
-  aircraftN: string
-  */
-	aircraftN: string
-	pilotFirstName: string
-	pilotLastName: string
-	pilotIdNumber: string
-	startDateTime: string
-	startGpsLocation: string
-	startAirport: string
-	endDateTime: string
-	endGpsLocation: string
-	endAirport: string
-}
-
-export interface Pilot {
-	firstName: string
-	lastName: string
-	idNumber: string
+	aircraftNNumber: string
+	pilotId: string
+	startNode: FlightNode
 }
 
 export interface FlightNode {
-	dateTime: string
-	gpsLocation: string
-	airport: string
+	dateTimeEpoc: number
+	gpsLatitude: string
+	gpsLongitude: string
+	airport?: string
 }
