48b474d567645ed74af9a2580da38fd752e8427e
Nazario A. Ayala <nazario@niztech.com>
2023-03-05 10:25:01 -0800

    Added endFlight

--- src/Controllers/flightController.ts
+++ src/Controllers/flightController.ts
@@ -1,5 +1,6 @@
 import { RouterContext } from 'oak/mod.ts'
 import { Flight } from '../Models/Flight.ts'
+import { FlightNode } from '../types.ts'
 
 export const getPilotsFlights = async (ctx: RouterContext) => {
 	const pilotId = ctx.params.pilotId
@@ -20,6 +21,32 @@ export const createFlight = async (ctx: RouterContext) => {
 	const id = await flightsCollection.insertOne(flightEntry)
 	ctx.response.status = 201
 	ctx.response.body = { id }
+}
+
+export const endFlight = async (ctx: RouterContext) => {
+	const flightEndNode = await ctx.request.body().value as FlightNode
+	const flightId = await ctx.params.flightId
+	const startedFlight = await Flight
+		.where('id', '=', flightId.trim().toString())
+		.where('endTime', '=', null)
+		.first()
+
+	if (!startedFlight) {
+		ctx.response.status = 403
+		ctx.response.body = { 'error': 'Could not end flight' }
+	}
+
+	try {
+		startedFlight.endTime = flightEndNode.dateTime
+		startedFlight.endGpsLatitude = flightEndNode.gpsLatitude
+		startedFlight.endGpsLongitude = flightEndNode.gpsLongitude
+		startedFlight.update()
+		ctx.response.status = 201
+		ctx.response.body = { 'message': 'End of flight recorded successfully' }
+	} catch (e) {
+		ctx.response.status = 403
+		ctx.response.body = { 'error': 'Could not end flight' }
+	}
 }
 
 export const getFlight = async (ctx: RouterContext) => {
